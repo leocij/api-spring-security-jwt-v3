d44ab22a4fc2a78ed993ec80c16bba1ff021cc6d
Leoci Melo <leocij@hotmail.com>
2019-05-19 15:49:58 -0300

    atualizado sql

--- src/main/resources/roles.sql
+++ src/main/resources/roles.sql
@@ -9,8 +9,20 @@ INSERT INTO roles(name) VALUES('ROLE_USER');
 INSERT INTO roles(name) VALUES('ROLE_PM');
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
 
+-- password: admin123
+INSERT INTO users (EMAIL, NAME, PASSWORD, USERNAME) VALUES ("admin@email.com", "Admin", "$2a$10$U85.QohgfuurJl6X8TXrtO/TYob/aK4FqxjR7mMXmedG6dIzIhKGe", "admin");
+
+INSERT INTO user_roles (USER_ID, ROLE_ID) values (1, 3);
+
+-- password: user123
+INSERT INTO users (EMAIL, NAME, PASSWORD, USERNAME) VALUES ("user@email.com", "User", "$2a$10$a5XZVqKG.eyVBCiRuBZzcu7UUjFhApQWyB4LW5ULoq1ewq1Uo38Ai", "user");
+
+INSERT INTO user_roles (USER_ID, ROLE_ID) values (2, 1);
+
 select * from roles;
 
 select * from users;
 
 select * from user_roles;
+
+
